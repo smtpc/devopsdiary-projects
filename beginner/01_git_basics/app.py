11530588db628f6e8c41fd7610a74bcd9a61c7b1
smtpc <smtpc2024@gmail.com>
2025-05-02 15:17:02 -0300

    get project title using requests

--- beginner/01_git_basics/app.py
+++ beginner/01_git_basics/app.py
@@ -2,6 +2,8 @@ import requests
 
 url = "https://devopsdiary.site"
 
-r = requests.get(url, timeout=5)
+r = requests.get(url, timeout=5).text
 
-print(r.text)
+title = r[r.find('<title>') + 7 : r.find('</title>')]
+
+print(title)
